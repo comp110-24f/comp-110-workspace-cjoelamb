a87fd33d403f5f26299d183977465202b42c889a
C. Joseph Lamb <clamb@unc.edu>
2024-08-27 12:17:29 -0400

    Finished Exercise 00!

---
+++ Lessons/functions.py
@@ -0,0 +1,7 @@
+def sum(num1: int, num2: int) -> int:
+    """Add two numbers together."""
+    return num1 + num2
+
+
+sum(num1=11, num2=3)
+print(sum(num1=11, num2=3))
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My First Exercise in COMP110!"""
+
+__author__ = "730521229"
+
+
+def greet(name: str) -> str:
+    """A Welcoming First Function Definition"""
+    return "Hello, " + name + "!"
